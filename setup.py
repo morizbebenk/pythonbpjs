6e52aa3bfdd7993b50ac7fa8a2874707d9dbfb0b
Moriz <morizbebenk@gmail.com>
2021-12-13 17:18:37 +0700

    Update version

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-requirements = ["lzstring", "requests", "pycryptodome"]
-
 setuptools.setup(
 	name="pythonbpjs",
-	version="0.0.2",
+	version="0.0.3",
 	author="Moriz",
 	author_email="morizbebenk@gmail.com",
 	description="Aplikasi Python yang digunakan untuk menangani proses dekripsi respon data dari bridging BPJS VCLAIM REST 2.0 (Encrypted Version). Support VCLAIM v1 dan API JKN (Antrean RS).",
@@ -20,16 +18,16 @@ setuptools.setup(
     classifiers=[
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    license='MIT',
-    keywords='bpjs vclaim',
-    install_requires=requirements
+    license="MIT",
+    keywords="bpjs vclaim vclaim-rest vclaim-encrypted ",
+    install_requires=["lzstring", "requests", "pycryptodome"]
 )
