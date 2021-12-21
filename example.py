e664020a856f1846fc8bf0bcc6d12c021a643003
Moriz <morizbebenk@gmail.com>
2021-12-21 11:29:16 +0700

    Update example.py

--- example.py
+++ example.py
@@ -6,14 +6,17 @@ from pythonbpjs import bpjs
 # Host Development API JKN : https://apijkn-dev.bpjs-kesehatan.go.id/antreanrs_dev/
 # Host Production API JKN : -
 
-host = "https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/"
-consid = ""
-secret = ""
-user_key = ""
-is_encrypt = "1"
+credential = {
+    "host": "https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/",
+    "consid": "",
+    "secret": "",
+    "user_key": "",
+    "is_encrypt": "1"
+}
+
 endpoint = "referensi/poli/ana"
 method = "get"
 payload = ""
 
-data = bpjs.bridging(host, consid, secret, user_key, is_encrypt, endpoint, method)
+data = bpjs.bridging(credential, endpoint, method, payload)
 print(data)
